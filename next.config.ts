a5ee2c8fdf892511a9d2501790f2d268fa0f9092
Shin Seungcheol <ani7981@gmail.com>
2025-12-31 16:38:33 +0900

    오류 제거 커밋!!!

--- next.config.ts
+++ next.config.ts
@@ -3,12 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactCompiler: true,
   reactStrictMode: true,
-
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "opengraph.githubassets.com",
+        pathname: "/**",
       },
     ],
     qualities: [75, 100],
--- src/pages/api/GithubApi.ts
+++ src/pages/api/GithubApi.ts
@@ -1,22 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-interface GithubRepo {
-  name: string;
-  html_url: string;
-}
-
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   const username = "shincharl";
-
   const response = await fetch(
     `https://api.github.com/users/${username}/repos`
   );
   const data = await response.json();
 
-  const projects = data.map((repo: GithubRepo) => ({
+  const projects = data.map((repo: any) => ({
     name: repo.name,
     url: repo.html_url,
     image: `https://opengraph.githubassets.com/1/${username}/${repo.name}`,
