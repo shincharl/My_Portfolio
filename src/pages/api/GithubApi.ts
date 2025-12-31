67db0222164188a62ceb758f20aff42239eab6b0
Shin Seungcheol <ani7981@gmail.com>
2025-12-31 16:32:55 +0900

    GithubApi.ts 변경 커밋!!!

---
+++ src/pages/api/GithubApi.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+interface GithubRepo {
+  name: string;
+  html_url: string;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const username = "shincharl";
+
+  const response = await fetch(
+    `https://api.github.com/users/${username}/repos`
+  );
+  const data = await response.json();
+
+  const projects = data.map((repo: GithubRepo) => ({
+    name: repo.name,
+    url: repo.html_url,
+    image: `https://opengraph.githubassets.com/1/${username}/${repo.name}`,
+  }));
+
+  res.status(200).json(projects);
+}
